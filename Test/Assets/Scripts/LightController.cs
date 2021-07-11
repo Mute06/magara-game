09f58e812e8c2f9422ee0a30485e28269fec8115
Mute06 <umutkanibir06@gmail.com>
2021-07-11 20:57:04 +0300

    Combat System

---
+++ Test/Assets/Scripts/LightController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public static LightController instance;
+
+    [SerializeField] private Light[] lights;
+
+    public Color redColor;
+    public Color blueColor;
+    public Color greenColor;
+    public Color yellowColor;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public ColorStates currentState;
+    public enum ColorStates
+    {
+        Red = 0,
+        Blue = 1,
+        Green = 2,
+        Yellow = 3
+    }
+
+    public void ChangeColorState(ColorStates nextState)
+    {
+        if (nextState == ColorStates.Red)
+        {
+            foreach (var item in lights)
+            {
+                item.color = redColor;
+            }
+        }
+        else if (nextState == ColorStates.Blue)
+        {
+            foreach (var item in lights)
+            {
+                item.color = blueColor;
+            }
+        }
+        else if (nextState == ColorStates.Green)
+        {
+            foreach (var item in lights)
+            {
+                item.color = redColor;
+            }
+        }
+        else if (nextState == ColorStates.Yellow)
+        {
+            foreach (var item in lights)
+            {
+                item.color = yellowColor;
+            }
+        }
+        currentState = nextState;
+
+    }
+
+
+}
---
+++ Test/Assets/Scripts/Robot.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Robot : MonoBehaviour
+{
+    public int health = 100;
+    public Slider healthBar;
+    public UnityEvent hitEffects;
+
+
+
+    public void UpdateHealth(int increasment)
+    {
+        health += increasment;
+        healthBar.value = health;
+    }
+
+    #region Hits
+    public void HitBlue()
+    {
+        if (LightController.instance.currentState == LightController.ColorStates.Blue)
+        {
+            UpdateHealth(-10);
+            hitEffects.Invoke();
+            if (LightController.instance.currentState == LightController.ColorStates.Yellow)
+            {
+                LightController.instance.ChangeColorState(0);
+            }
+            else
+            {
+                LightController.instance.ChangeColorState(LightController.instance.currentState + 1);
+            }
+        }
+    }
+    public void HitRed()
+    {
+        if (LightController.instance.currentState == LightController.ColorStates.Red)
+        {
+            UpdateHealth(-10);
+            hitEffects.Invoke();
+            if (LightController.instance.currentState == LightController.ColorStates.Yellow)
+            {
+                LightController.instance.ChangeColorState(0);
+            }
+            else
+            {
+                LightController.instance.ChangeColorState(LightController.instance.currentState + 1);
+            }
+        }
+    }
+
+    public void HitGreen()
+    {
+        if (LightController.instance.currentState == LightController.ColorStates.Green)
+        {
+            UpdateHealth(-10);
+            hitEffects.Invoke();
+            if (LightController.instance.currentState == LightController.ColorStates.Yellow)
+            {
+                LightController.instance.ChangeColorState(0);
+            }
+            else
+            {
+                LightController.instance.ChangeColorState(LightController.instance.currentState + 1);
+            }
+        }
+    }
+
+    public void HitYellow()
+    {
+        if (LightController.instance.currentState == LightController.ColorStates.Yellow)
+        {
+            UpdateHealth(-10);
+            hitEffects.Invoke();
+            if (LightController.instance.currentState == LightController.ColorStates.Yellow)
+            {
+                LightController.instance.ChangeColorState(0);
+            }
+            else
+            {
+                LightController.instance.ChangeColorState(LightController.instance.currentState + 1);
+            }
+        }
+    }
+    #endregion
+}
