7fe8b44350259721e510fdbb5ac2173d010bb2ef
talhanuhuymaz <talha_nuh@hotmail.com>
2021-07-12 12:11:48 +0300

    UI updates

--- Test/Assets/Scripts/Menu.cs
+++ Test/Assets/Scripts/Menu.cs
@@ -7,11 +7,20 @@ public class Menu : MonoBehaviour
 {
     public void PlayGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-      }
+    }
     public void QuitGame()
     {
         Debug.Log("Oundan Cikildi");
         Application.Quit();
     }
 
+    public void CredidtsGame()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
